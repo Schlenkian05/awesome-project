6fa9471caecc1578d2744f423689d59882969e9f
Schlenkian05 <carthickpalraj@gmail.com>
2023-01-25 23:36:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('new connection')
-    res.end('Hello Awesome')
+    res.end('Hello Vairavan')
 })
 
 const PORT = process.env.PORT || 8080
